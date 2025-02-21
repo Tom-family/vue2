0cc710289f4375405aad7900d830b1c8b8265c9c
sunsheng <941216988@qq.com>
2025-02-21 10:36:05 +0800

    vuex-persistedstate  持有化

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persistedstate";
 import user from "./modules/user.js";
-
 Vue.use(Vuex);
+
+const persistedStatePaths = ["user.count"];
 
 export default new Vuex.Store({
   modules: {
     user,
   },
+  plugins: [
+    VuexPersistence({
+      paths: persistedStatePaths,
+    }),
+  ],
 });
